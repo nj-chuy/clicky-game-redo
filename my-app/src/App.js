5a06a5e6ebd47f62c1c4be63307a47d6e529dcc0
jesus garcia <nj.chuy@gmail.com>
2019-02-04 22:36:45 -0500

    cookie

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   state = {
     friends,
     score: 0,
-    highScore: 0,
+    highScore: 0
   };
 
   // removeFriend = id => {
@@ -19,65 +19,78 @@ class App extends Component {
   //   this.setState({ friends });
   // };
 
-  //https://www.frankmitchell.org/2015/01/fisher-yates///
-   shuffle = (array) => {
-    var i = 0
-      , j = 0
-      , temp = null
-  
-    for (i = array.length - 1; i > 0; i -= 1) {
-      j = Math.floor(Math.random() * (i + 1))
-      temp = array[i]
-      array[i] = array[j]
-      array[j] = temp
-    }
+  componentDidMount() {
+    //this.setState({friends: this.shuffle(this.state.friends)});
   }
 
-  
+  //https://www.frankmitchell.org/2015/01/fisher-yates///
+  shuffle = array => {
+    var i = 0,
+      j = 0,
+      temp = null;
+
+    for (i = array.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
 
   beenClicked = name => {
-     const thisFriend = this.state.friends.filter(friend => friend.name === name )[0]
-    if(thisFriend.clicked[0]){
-      alert("GAME OVER, YOU ALREADY TRIED TO EAT THAT COOKIE")
+    console.log("clicked");
+    const thisFriend = this.state.friends.filter(
+      friend => friend.name === name
+    )[0];
+    if (thisFriend.clicked) {
+      alert("GAME OVER, YOU ALREADY TRIED TO EAT THAT COOKIE");
 
       // reset all clicked values to false
-      this.state.friends.filter(friend => friend.clicked === true).map(friend => friend.clicked = false);
+      this.state.friends
+        .filter(friend => friend.clicked === true)
+        .map(friend => (friend.clicked = false));
       // set current score back to zero
       this.setState({
         //pass the current friends array into the shuffle function, to generate a new shuffle array (setState will cause new render)
         friends: this.shuffle(this.state.friends),
         score: 0
-      })
-    }
-    else{
-      //change click flag 
+      });
+    } else {
+      //change click flag
+      thisFriend.clicked = true;
+      const newScore = this.state.score + 1;
+      const highScore =
+        newScore > this.state.highScore ? newScore : this.state.highScore;
       this.setState({
-        clicked: true, 
-        score: + 1,
+        score: newScore,
         friends: this.shuffle(this.state.friends),
-      })
+        highScore: highScore
+      });
       // add a point to score
-      // shuffle deck
+
       // score > highscore (add up on highscore)
     }
-    
-  }
-
-
+  };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Clicky the Cookie Game</Title>
+        <div>
+          <h1>Score: {this.state.score}</h1>
+          <h1>High Score: {this.state.highScore}</h1>
+        </div>
         {this.state.friends.map(friend => (
           <FriendCard
-            beenClicked ={this.beenClicked}
+            beenClicked={this.beenClicked}
             id={friend.id}
             key={friend.id}
             name={friend.name}
             image={friend.image}
             clicked={friend.clicked}
+            anything={10}
           />
         ))}
       </Wrapper>
